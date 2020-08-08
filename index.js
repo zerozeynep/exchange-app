cb7ab7229fe93b72e8824ee43e65cce038ec505c
zerozeynep <zeynep-demirtas@windowslive.com>
2020-08-08 22:39:55 +0300

    display rates at html

--- index.js
+++ index.js
@@ -3,7 +3,19 @@ setInterval(getRates,10000)
 
 function getRates(){
   $.getJSON('http://data.fixer.io/api/latest?access_key=d5062fe35bfcd2277d29b6dc3462262d&symbols=USD&format=1', function(response){
-    console.log(response)
+    if(response.success){
+      console.log(response)
+      $('#result').text('1 EUR : '+ response.rates.USD + ' USD')
+
+    } else {
+      $('#result').html(`
+                        <div>API bir hata ile karsilasti. 30 sn bekleyin ya da tekrar istek gondermek icin butona tiklayin.
+                          <div class='error-info' >Hata detaylari : ${response.error.info}</div>
+                          <button id=resend>tekrar dene</button>
+                        </div>`)
+                        
+    }
+   
   })
 }
 
